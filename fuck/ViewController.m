7a40c36d3e6b91db3257db8c25c67156c242c5bb
Levan <15882375278@163.com>
2016-08-31 21:31:35 +0800

    新建项目

---
+++ fuck/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  fuck
+//
+//  Created by 甘淋方 on 16/8/31.
+//  Copyright © 2016年 甘淋方. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"fuck you!");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
